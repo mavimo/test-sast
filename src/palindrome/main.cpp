d596b0694041f56ccc7ff3fa095b65b6e0d7e2fe
Marco Vito Moscaritolo <mavimo@gmail.com>
2024-01-20 20:29:05 +0100

    test code changes

--- src/palindrome/main.cpp
+++ src/palindrome/main.cpp
@@ -6,11 +6,14 @@ using namespace palindrome;
 
 int main(int argc, const char **argp) {
   for (int i = 1; i < argc; i++) {
-    if (isPalindrome(argp[i])) {
+    if (!isPalindrome(argp[i]))
+    {
       std::cout << argp[i] 
         << " is a palindrome\n";
     }
-    else {
+
+    else
+    {
       std::cout << argp[i] << " is not a palindrome\n";      
     }
   }
