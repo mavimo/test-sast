5b388698fca3cc0349419f5b4e3a46aa98899d80
Marco Vito Moscaritolo <mavimo@gmail.com>
2024-01-27 13:04:46 +0100

    WIP

--- src/palindrome/main.cpp
+++ src/palindrome/main.cpp
@@ -6,14 +6,11 @@ using namespace palindrome;
 
 int main(int argc, const char **argp) {
   for (int i = 1; i < argc; i++) {
-    if (!isPalindrome(argp[i]))
-    {
+    if (isPalindrome(argp[i])) {
       std::cout << argp[i] 
         << " is a palindrome\n";
     }
-
-    else
-    {
+    else {
       std::cout << argp[i] << " is not a palindrome\n";      
     }
   }
